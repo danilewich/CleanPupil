1b19d9c1a0571e875e466c36f645941cde14e756
Vitliy <89633573351@yandex.ru>
2019-12-13 23:04:04 +0300

    words

--- app/src/main/java/com/danilevich/pupil/domain/entity/Words.java
+++ app/src/main/java/com/danilevich/pupil/domain/entity/Words.java
@@ -1,4 +1,14 @@
-package com.danilevich.pupil.domain;
+package com.danilevich.pupil.domain.entity;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "WORDS")
 public class Words {
+
+    @DatabaseField(columnName = "id", generatedId = true)
+    int id;
+    @DatabaseField(columnName = "name")
+    String name;
+
 }
